5cd5da82fe9415e6e5223452d4e21edfd4b63cc5
Alexander Trost <Galexrt@googlemail.com>
2015-11-11 12:46:30 +0100

    This should fix the postgres user authentication fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ DB_PASS=${DB_PASS:-}
 DB_LOCALE=${DB_LOCALE:-C}
 DB_UNACCENT=${DB_UNACCENT:false}
 DB_TSEARCH_EXT=${DB_TSEARCH_EXT:true}
-DB_ROOT_PASS=${DB_ROOT_PASS:-$DB_PASS}
+DB_ROOT_PASS=${DB_ROOT_PASS:-$(echo $DB_PASS)}
 # by default postgresql will start up as a standalone instance.
 # set this environment variable to master, slave or snapshot to use replication features.
 # "snapshot" will create a point in time backup of a master instance.
@@ -184,6 +184,12 @@ if [[ -n ${PG_OLD_VERSION} ]]; then
     -O "-c config_file=${PG_CONFDIR}/postgresql.conf" >/dev/null
 fi
 
+if [[ -n $DB_ROOT_PASS ]]; then
+    echo "ALTER USER postgres '${DB_ROOT_PASS}';" |
+      sudo -Hu ${PG_USER} ${PG_BINDIR}/postgres --single \
+        -D ${PG_DATADIR} -c config_file=${PG_CONFDIR}/postgresql.conf >/dev/null
+fi
+
 # Hot standby (slave and snapshot) servers can ignore the following code.
 if [[ ${PSQL_MODE} == standalone || ${PSQL_MODE} == master ]]; then
   if [[ -n ${REPLICATION_USER} ]]; then
@@ -245,12 +251,6 @@ if [[ ${PSQL_MODE} == standalone || ${PSQL_MODE} == master ]]; then
       fi
     done
   fi
-fi
-
-if [[ -n $DB_ROOT_PASS ]]; then
-    echo "ALTER USER postgres '${DB_ROOT_PASS}';" |
-      sudo -Hu ${PG_USER} ${PG_BINDIR}/postgres --single \
-        -D ${PG_DATADIR} -c config_file=${PG_CONFDIR}/postgresql.conf >/dev/null
 fi
 
 echo "Starting PostgreSQL server..."
